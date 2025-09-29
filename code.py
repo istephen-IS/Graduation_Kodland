06ec1bdd2c957ad7a427ad9588dc88d6c2b02326
istephen-IS <sensen09092013@gmail.com>
2025-09-29 19:54:13 +0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,167 @@
+# test-bot(bot class)
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
+import discord
+import random
+import os
+import requests
+from discord.ext import commands
+from bot_logic import gen_pass
+from model import get_class
+from detect_objects import detect
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+# command prefix 
+bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})') # type: ignore
+    print('------')
+
+# adding two numbers
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+# multiplying two numbers
+@bot.command()
+async def times(ctx, left: int, right: int):
+    """Times two numbers together."""
+    await ctx.send(left * right)
+
+# dividing two numbers
+@bot.command()
+async def div(ctx, left: int, right: int):
+    """Divides two numbers together."""
+    await ctx.send(left / right)
+
+# minusing two numbers
+@bot.command()
+async def min(ctx, left: int, right: int):
+    """Minus two numbers together."""
+    await ctx.send(left - right)
+
+# squaring two numbers
+@bot.command()
+async def sq(ctx, left: int, right: int):
+    """Squares two numbers together."""
+    await ctx.send(left ** right)
+
+
+#Computer Vision Classification Pipit/Merpati
+@bot.command()
+async def klasifikasi(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            #file_url = attachment.url IF URL
+            await attachment.save(f"./images/{file_name}")
+            await ctx.send(get_class(model_path="keras_model.h5", labels_path="labels.txt", image_path=f"./images/{file_name}"))
+    else:
+        await ctx.send("Anda lupa mengunggah gambar :(")
+
+#Computer Vision Deteksi objek
+@bot.command()
+async def deteksi(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            #file_url = attachment.url IF URL
+            await attachment.save(f"./images2/{file_name}")
+            await ctx.send(detect(input_image=f"./images2/{file_name}", output_image=f"./images2/{file_name}", model_path="yolov3.pt"))
+            with open(f'images2/{file_name}', 'rb') as f:
+                picture = discord.File(f)
+            await ctx.send(file=picture)
+    else:
+        await ctx.send("Anda lupa mengunggah gambar :(")
+
+@bot.command()
+async def reverse(ctx, *, x: str):
+    y = x
+    await ctx.send(' '.join(y.split()[::-1]))
+
+@bot.command()
+async def tulis(ctx, *, my_string: str):
+    with open('kalimat.txt', 'w', encoding='utf-8') as t:
+        text = ""
+        text += my_string
+        t.write(text)
+@bot.command()
+async def tambahkan(ctx, *, my_string: str):
+    with open('kalimat.txt', 'a', encoding='utf-8') as t:
+        text = "\n"
+        text += my_string
+        t.write(text)
+@bot.command()
+async def baca(ctx):
+    with open('kalimat.txt', 'r', encoding='utf-8') as t:
+        document = t.read()
+        await ctx.send(document)
+
+# meme
+@bot.command()
+async def meme(ctx):
+    # try by your self 2 min
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+ 
+    await ctx.send(file=picture)
+
+# spamming word
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        
+# password generator        
+@bot.command()
+async def pw(ctx):
+    await ctx.send(f'Kata sandi yang dihasilkan: {gen_pass(10)}')
+@bot.command()
+async def bye(ctx):
+    await ctx.send('\U0001f642')
+# coinflip
+@bot.command()
+async def coinflip(ctx):
+    num = random.randint(1,2)
+    if num == 1:
+        await ctx.send('It is Head!')
+    if num == 2:
+        await ctx.send('It is Tail!')
+    
+# rolling dice
+@bot.command()
+async def dice(ctx):
+    nums = random.randint(1,6)
+    if nums == 1:
+        await ctx.send('It is 1!')
+    elif nums == 2:
+        await ctx.send('It is 2!')
+    elif nums == 3:
+        await ctx.send('It is 3!')
+    elif nums == 4:
+        await ctx.send('It is 4!')
+    elif nums == 5:
+        await ctx.send('It is 5!')
+    elif nums == 6:
+        await ctx.send('It is 6!')
+
+# welcome message
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}') # type: ignore
+
+
